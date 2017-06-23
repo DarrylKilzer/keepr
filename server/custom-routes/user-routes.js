6f0c3a1afe37404b6b6b4ee48d226d7a307f5680
Darryl Kilzer <darrylkilzer@gmail.com>
2017-06-23 13:57:21 -0600

    model adjustments

--- server/custom-routes/user-routes.js
+++ server/custom-routes/user-routes.js
@@ -1,15 +1,69 @@
+let Vaults = require('../models/vault')
+let Keeps = require('../models/keep')
+
 export default {
-  
+  getKeepsByVaultId: {
+    path: '/vaults/:vaultId/keeps',
+    reqType: 'get',
+    method(req, res, next) {
+      let action = 'return vault and associated keeps'
+      Vaults.findById(req.params.vaultId)
+        .then(vault => {
+          Lists.find({ vaultId: req.params.vaultId })
+            .then(keeps => {
+              vault.keeps = keeps
+              res.send(handleResponse(action, vault.keeps))
+            })
+        }).catch(error => {
+          return next(handleResponse(action, null, error))
+        })
+    }
+  },
+  getKeepsByUserId: {
+    path: '/userkeeps/',
+    reqType: 'get',
+    method(req, res, next) {
+      let action = 'return user associated keeps'
+      Keeps.find({ userId: req.session.uid })
+        .then(keeps => {
+          keeps = keeps
+          res.send(handleResponse(action, keeps))
+        }).catch(error => {
+          return next(handleResponse(action, null, error))
+        })
+    }
+  },
+  getKeeps: {
+    path: '/keeps',
+    reqType: 'get',
+    method(req, res, next) {
+      let action = 'Find keeps'
+      Keeps.find()
+        .then(keeps => {
+          var publicKeeps = []
+          for (var i = 0; i < keeps.length; i++) {
+            var keep = keeps[i]
+            if (keep.private == false) {
+              publicKeeps.push(keep)
+            }
+          }
+          publicKeeps.push({ name: "keep" })
+          res.send(handleResponse(action, publicKeeps))
+        }).catch(error => {
+          return next(handleResponse(action, null, error))
+        })
+    }
+  }
 }
 
-
 function handleResponse(action, data, error) {
-    var response = {
-      action: action,
-      data: data
-    }
-    if (error) {
-      response.error = error
-    }
-    return response
+  var response = {
+    action: action,
+    data: data
   }
+  if (error) {
+    response.error = error
+  }
+  return response
+}
+        // {creatorId: req.session.uid}
--- server/models/keep.js
+++ server/models/keep.js
@@ -3,11 +3,18 @@ let mongoose = require('mongoose')
 let ObjectId = mongoose.Schema.ObjectId
 
 var schema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
+  title: { type: String, required: true },
+  imageURL: { type: String, required: true }, 
+  body: { type: String, required: true }, 
+  tags: [{ type: String, required: true }],
+  vaults: [{type: ObjectId, ref: models.vault.name}],
+  articleURL: { type: String, required: true }, 
   created: { type: Number, default: Date.now() },
   creatorId: { type: ObjectId, ref: models.user.name, required: true },
-  timesAdded: { type: Number, default: 0 }
+  keepCount: { type: Number, default: 0 },
+  shareCount: { type: Number, default: 0 },
+  viewCount: { type: Number, default: 0 },
+  private: {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model(models.keep.name, schema);
